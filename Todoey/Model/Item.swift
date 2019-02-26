9e6d909b71d41a264a776cf3fc5c28e1eeb97f56
Tomás Middleton <tomas@middleton.cl>
2019-02-26 18:13:01 -0300

    Convert to MVC Design Pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tomás Middleton on 2/26/19.
+//  Copyright © 2019 Tomás Middleton. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
